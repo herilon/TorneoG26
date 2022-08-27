13cfffec1af171b84df00641f3ef32b0a34babc6
Heri <hlondono@misena.edu.co>
2022-08-27 16:52:41 -0500

    Consola AddMunicipio

---
+++ Torneo.App.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+namespace Torneo.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+
+        static void Main(string[] args)
+        {
+            AddMunicipio();
+        }
+
+        private static void AddMunicipio()
+        {
+            var municipio = new Municipio
+            {
+                Nombre = "Manizales",
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
+    }
+}
