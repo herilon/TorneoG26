92c27526c92b193334278b7559aec619b961d326
Heri <hlondono@misena.edu.co>
2022-08-27 16:39:49 -0500

    Repositorio AddMunicipio

---
+++ Torneo.App.Persistencia/AppRepositorios/RepositorioMunicipio.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Torneo.App.Dominio;
+namespace Torneo.App.Persistencia
+{
+    public class RepositorioMunicipio : IRepositorioMunicipio
+    {
+        private readonly DataContext _dataContext = new DataContext();
+
+        public Municipio AddMunicipio(Municipio municipio)
+        {
+            var municipioInsertado = _dataContext.Municipios.Add(municipio);
+            _dataContext.SaveChanges();
+            return municipioInsertado.Entity;
+        }
+    }
+}
