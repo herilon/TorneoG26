579bab06fee7121f423a934a3c3cfecb14dbfd1e
Heri <hlondono@misena.edu.co>
2022-08-31 21:12:33 -0500

    Consola Municipio Menu

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -8,7 +8,33 @@ namespace Torneo.App.Consola
 
         static void Main(string[] args)
         {
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1. Insertar Municipio");
+                Console.WriteLine("0. Salir");
+                opcion = Int32.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        AddMunicipio();
+                        break;
+                }
+            } while (opcion != 0);
+            
+            
+            
         }
 
+        private static void AddMunicipio()
+        {
+            Console.WriteLine("Ingrese el nombre del Municipio");
+            string nombre = Console.ReadLine();
+            var municipio = new Municipio
+            {
+                Nombre = nombre,
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
     }
 }
