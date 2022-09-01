789a393a5e4502d47a8d4c0deb8304f08f3b6191
Heri <hlondono@misena.edu.co>
2022-08-31 20:53:24 -0500

    Consola clean

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -4,20 +4,10 @@ namespace Torneo.App.Consola
 {
     class Program
     {
-        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
 
         static void Main(string[] args)
         {
-            AddMunicipio();
         }
 
-        private static void AddMunicipio()
-        {
-            var municipio = new Municipio
-            {
-                Nombre = "Manizales",
-            };
-            _repoMunicipio.AddMunicipio(municipio);
-        }
     }
 }
--- Torneo.App.Persistencia/AppRepositorios/IRepositorioMunicipio.cs
+++
@@ -1,8 +0,0 @@
-using Torneo.App.Dominio;
-namespace Torneo.App.Persistencia
-{
-    public interface IRepositorioMunicipio
-    {
-        public Municipio AddMunicipio(Municipio municipio);
-    }
-}
