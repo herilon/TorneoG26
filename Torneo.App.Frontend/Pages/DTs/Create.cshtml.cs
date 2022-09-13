556c8a5acb426586d35e310faa63b5fcc780fb7d
Heri <hlondono@misena.edu.co>
2022-09-12 21:02:12 -0500

    Create DT

--- Torneo.App.Frontend/Pages/DTs/Create.cshtml.cs
+++ Torneo.App.Frontend/Pages/DTs/Create.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.DTs
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioDT _repoDT;
+        public DirectorTecnico dt {get; set;}
+
+        public CreateModel(IRepositorioDT repoDT)
+        {
+            _repoDT = repoDT;
+        }
+
         public void OnGet()
         {
+            dt = new DirectorTecnico();
+        }
+
+        public IActionResult OnPost(DirectorTecnico dt)
+        {
+            _repoDT.AddDT(dt);
+            return RedirectToPage("Index");
         }
     }
 }
